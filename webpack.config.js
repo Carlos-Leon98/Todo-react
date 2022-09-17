acb826397216d2bb573fa7933ba5e3b0089209ce
Carlos Leon <carlosaleonrivas@gmail.com>
2022-09-17 03:19:13 -0500

    Setting up webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.html$/i,
+                use: "html-loader"
+            }
+        ]
+    },
+    plugin: [
+        new HtmlWebpackPlugin()
+    ]
+}
